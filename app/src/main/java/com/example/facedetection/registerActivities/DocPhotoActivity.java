e883a62fc3881a7c1aa925aee75d1457316b5544
ale-neto <alexandreneto.dev@gmail.com>
2020-08-10 11:12:52 -0300

    update

--- app/src/main/java/com/example/facedetection/registerActivities/DocPhotoActivity.java
+++ app/src/main/java/com/example/facedetection/registerActivities/DocPhotoActivity.java
@@ -6,17 +6,15 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.example.facedetection.R;
-import com.example.facedetection.recognition.PhotoFragment;
 
 import butterknife.ButterKnife;
 
-public class RegisterActivity extends AppCompatActivity  implements PhotoFragment.OnFragmentInteractionListener {
+public class DocPhotoActivity extends AppCompatActivity  implements DocPhotoFragment.OnFragmentInteractionListener {
 
     String token;
     int PERMISSION_ALL = 1;
@@ -28,22 +26,21 @@ public class RegisterActivity extends AppCompatActivity  implements PhotoFragmen
     };
 
     @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_photo);
+        setContentView(R.layout.activity_doc_photo);
 
         Bundle resultB = getIntent().getExtras();
         token = resultB.getString("register");
 
         ButterKnife.bind(this);
         checkPermissions();
-    }
-    
-
-    @Override
-    public void onBackPressed() {
-        finish();
     }
 
     void checkPermissions() {
@@ -56,13 +53,13 @@ public class RegisterActivity extends AppCompatActivity  implements PhotoFragmen
         }
         flagPermissions = true;
 
-        PhotoFragmentFisrt photoFragmentFisrt = new PhotoFragmentFisrt();
+        DocPhotoFragment photoFragment = new DocPhotoFragment();
         Bundle result =  new Bundle();
         result.putString("token", token);
-        photoFragmentFisrt.setArguments(result);
+        photoFragment.setArguments(result);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.res_photo_layout, photoFragmentFisrt)
+                .replace(R.id.res_photo_layout,  photoFragment)
                 .addToBackStack(null)
                 .commit();
     }
